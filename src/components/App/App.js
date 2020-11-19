dc2617fed5b929e18664294e9a9e5992d97435bd
Ed Ignot <edignot@gmail.com>
2020-11-19 16:09:03 -0700

    Header styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import RestaurantsPage from '../../pages/RestaurantsPage/RestaurantsPage'
-import './App.css'
 
 function App() {
   return (
