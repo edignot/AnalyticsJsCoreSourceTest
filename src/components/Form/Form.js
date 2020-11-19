5ba6540f5e17e20eb8c87a7c18cd56f3c29ae544
Ed Ignot <edignot@gmail.com>
2020-11-19 11:38:56 -0700

    Filtering drop down options

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { BiSearch } from 'react-icons/bi'
-import { clearFilter, filterRestaurants } from '../../actions/session'
+import { clearSearch, filterRestaurants } from '../../actions/session'
+import Dropdown from '../Dropdown/Dropdown'
 
 const Form = () => {
   const dispatch = useDispatch()
-
   const restaurants = useSelector((store) => store.restaurants)
+  const session = useSelector((store) => store.session)
 
   const [searchValue, setSearchValue] = useState('')
 
   useEffect(() => {
-    searchValue === '' && dispatch(clearFilter())
+    searchValue === '' && dispatch(clearSearch())
   }, [searchValue])
 
   const handleChange = (e) => {
@@ -34,8 +35,34 @@ const Form = () => {
 
   const clearHandler = () => {
     setSearchValue('')
-    dispatch(clearFilter())
+    dispatch(clearSearch())
   }
+
+  const restaurantsToFilter = session.searchApplied
+    ? restaurants.filter((restaurant) =>
+        session.filteredRestaurants.includes(restaurant.id),
+      )
+    : restaurants
+
+  const stateFilterOptions = restaurantsToFilter.reduce(
+    (options, restaurant) => {
+      !options.includes(restaurant.state) && options.push(restaurant.state)
+      return options
+    },
+    [],
+  )
+
+  const genreFilterOptions = restaurantsToFilter.reduce(
+    (options, restaurant) => {
+      restaurant.genreArray.forEach(
+        (genre) => !options.includes(genre) && options.push(genre),
+      )
+      return options
+    },
+    [],
+  )
+
+  console.log(genreFilterOptions)
 
   return (
     <section>
@@ -52,6 +79,8 @@ const Form = () => {
         </button>
         <button onClick={clearHandler}>CLEAR</button>
       </section>
+      <Dropdown possibleOptions={[]} title='Filter by State' />
+      <Dropdown possibleOptions={[]} title='Filter by Genre' />
     </section>
   )
 }
