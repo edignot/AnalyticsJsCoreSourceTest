bdbd97a13c1aff2b62440782029250e1bcfaec2e
Ed Ignot <edignot@gmail.com>
2020-11-18 18:25:26 -0700

    Converting string list data to array data in api call

--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -3,9 +3,18 @@ import * as api from '../api'
 export const getRestaurants = () => async (dispatch) => {
   try {
     const { data } = await api.fetchRestaurants()
+    const restaurants = data.map((restaurant) => {
+      const genreArray = restaurant.genre.split(',')
+      const tagsArray = restaurant.tags.split(',')
+      return {
+        ...restaurant,
+        genreArray,
+        tagsArray,
+      }
+    })
     dispatch({
       type: 'FETCH_RESTAURANTS',
-      payload: data,
+      payload: restaurants,
     })
   } catch (error) {
     console.log(error)
