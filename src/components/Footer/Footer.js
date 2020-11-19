9dedea42d49465a617074f382b263c4c585523e6
Ed Ignot <edignot@gmail.com>
2020-11-19 16:17:55 -0700

    Footer styled

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,13 @@ import React from 'react'
 import './Footer.css'
 
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer>
+      <p className='footer-text'>
+        &copy; Restaurants Search Engine. All rights reserved.
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
