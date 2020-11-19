1179f43d603655c893718a430c5aeb79ede37def
Ed Ignot <edignot@gmail.com>
2020-11-18 18:57:12 -0700

    readme initialized

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,16 @@
+import React, { useState } from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import logo from '../../logo.svg'
 import './App.css'
 import Restaurants from '../Restaurants/Restaurants'
+import Layout from '../Layout/Layout'
 
 function App() {
   return (
-    <>
+    <Layout>
       <img src={logo} className='App-logo' alt='logo' />
       <Restaurants />
-    </>
+    </Layout>
   )
 }
 
