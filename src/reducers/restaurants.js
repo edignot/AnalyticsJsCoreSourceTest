c01393eee856cc8ee4d9c6b44ea536be0f7ca8fe
Ed Ignot <edignot@gmail.com>
2020-11-18 17:27:25 -0700

    Saving fetched restaurants data to Redux store

--- src/reducers/restaurants.js
+++ src/reducers/restaurants.js
@@ -1,7 +1,7 @@
 export default (restaurants = [], action) => {
   switch (action.type) {
     case 'FETCH_RESTAURANTS':
-      return restaurants
+      return [...restaurants, ...action.payload]
     default:
       return restaurants
   }
