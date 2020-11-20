8384d51060c30f2a0e69ac6a16c509b8b800a2b3
Ed Ignot <edignot@gmail.com>
2020-11-20 10:22:52 -0700

    All reducers tested

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -3,20 +3,27 @@ import { restaurants } from './restaurants'
 import { session } from './session'
 
 describe('Reducers', () => {
-  let restaurantsTestData,
-    sessionTestData,
-    initialRestaurantsState,
-    initialSessionState
+  let restaurantsTestData, sessionTestData, restaurantsState, sessionState
 
   beforeEach(() => {
-    initialRestaurantsState = []
-    initialSessionState = {
+    restaurantsState = []
+
+    sessionState = {
       searchApplied: false,
       filteredRestaurants: [],
       currentPageNumber: 1,
       genreFilter: '',
       stateFilter: '',
       attireFilter: '',
+    }
+
+    sessionTestData = {
+      searchApplied: true,
+      filteredRestaurants: ['1', '2'],
+      currentPageNumber: 1,
+      genreFilter: 'genre1',
+      stateFilter: 'state1',
+      attireFilter: 'attire1',
     }
 
     restaurantsTestData = [
@@ -31,7 +38,7 @@ describe('Reducers', () => {
         lat: '1',
         long: '1',
         name: 'name1',
-        state: 'state',
+        state: 'state1',
         tags: 'tagA,tagB',
         tagsArray: ['tagA', 'tagB'],
         telephone: '1',
@@ -49,7 +56,7 @@ describe('Reducers', () => {
         lat: '2',
         long: '2',
         name: 'name2',
-        state: 'state',
+        state: 'state2',
         tags: 'tagA,tagB',
         tagsArray: ['tagA', 'tagB'],
         telephone: '2',
@@ -64,7 +71,88 @@ describe('Reducers', () => {
       type: 'FETCH_RESTAURANTS',
       restaurantsSortedByName: restaurantsTestData,
     }
-    const result = restaurants(initialRestaurantsState, action)
+    const result = restaurants(restaurantsState, action)
     expect(result).toEqual(restaurantsTestData)
+  })
+
+  it('should return session state with filtered restaurants info', () => {
+    const action = {
+      type: 'FILTER_RESTAURANTS',
+      filteredRestaurants: sessionTestData.filteredRestaurants,
+    }
+    const result = session(sessionState, action)
+    expect(result).toEqual({
+      ...sessionState,
+      currentPageNumber: 1,
+      searchApplied: true,
+      filteredRestaurants: sessionTestData.filteredRestaurants,
+    })
+  })
+
+  it('should return session state with cleared searched', () => {
+    const action = {
+      type: 'CLEAR_SEARCH',
+    }
+    const result = session(
+      { ...sessionState, genreFilter: sessionTestData.genreFilter },
+      action,
+    )
+    expect(result).toEqual({
+      ...sessionState,
+      genreFilter: sessionTestData.genreFilter,
+    })
+  })
+
+  it('should return session state with cleared session info', () => {
+    const action = {
+      type: 'CLEAR_ALL',
+    }
+    const result = session(sessionTestData, action)
+    expect(result).toEqual(sessionState)
+  })
+
+  it('should return session state with updated current page number', () => {
+    const action = {
+      type: 'SET_CURRENT_PAGE_NUMBER',
+      currentPageNumber: 3,
+    }
+    const result = session(sessionState, action)
+    expect(result).toEqual({ ...sessionState, currentPageNumber: 3 })
+  })
+
+  it('should return session state with updated genre filter', () => {
+    const action = {
+      type: 'SET_GENRE_FILTER',
+      genreFilter: sessionTestData.genreFilter,
+    }
+    const result = session(sessionState, action)
+    expect(result).toEqual({
+      ...sessionState,
+      genreFilter: sessionTestData.genreFilter,
+    })
+  })
+
+  it('should return session state with updated state filter', () => {
+    const action = {
+      type: 'SET_STATE_FILTER',
+      stateFilter: sessionTestData.stateFilter,
+    }
+    const result = session(sessionState, action)
+    expect(result).toEqual({
+      ...sessionState,
+      stateFilter: sessionTestData.stateFilter,
+    })
+  })
+
+  it('should return session state with updated attire filter', () => {
+    const action = {
+      type: 'SET_ATTIRE_FILTER',
+      attireFilter: sessionTestData.attireFilter,
+    }
+    const result = session(sessionState, action)
+    expect(result).toEqual({
+      ...sessionState,
+      attireFilter: sessionTestData.attireFilter,
+    })
   })
 })
