b93e80abe218fbaef26108f678da65b827c403f4
Ed Ignot <edignot@gmail.com>
2020-11-20 12:52:38 -0700

    App Unit test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,30 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import { initialStateTestData } from '../../test-data'
+import App from './App'
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('Restaurant', () => {
+  let AppComponent, store, mockStore, initialState, restaurant
+
+  beforeEach(() => {
+    initialState = initialStateTestData
+    restaurant = initialStateTestData.restaurants[0]
+
+    mockStore = configureStore()
+    store = mockStore(initialState)
+
+    AppComponent = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    )
+  })
+
+  it('App should successfully render Layout ( Layouts render Header, Footer and Children components )', () => {
+    const { getByText } = AppComponent
+    expect(getByText(restaurant.name)).toBeInTheDocument()
+  })
+})
