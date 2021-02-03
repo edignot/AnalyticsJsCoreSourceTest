7f8fc44510c0b4504278bd9ba9c3d583cf4a0a8f
Edita Ignotaite <edita.ignotaite@Editas-MacBook-Pro.local>
2021-02-03 11:25:45 -0700

    Initial commit

--- src/pages/RestaurantsPage/RestaurantsPage.js
+++ src/pages/RestaurantsPage/RestaurantsPage.js
@@ -2,8 +2,26 @@ import React from 'react'
 import Restaurants from '../../containers/Restaurants/Restaurants'
 import Form from '../../containers/Form/Form'
 import './RestaurantsPage.css'
+import Analytics from "@segment/analytics.js-core/build/analytics";
+import SegmentIntegration from "@segment/analytics.js-integration-segmentio";
+const analytics = new Analytics();
+analytics.use(SegmentIntegration);
+
+const integrationSettings = {
+  "Segment.io": {
+    apiKey: "NJcDKOk8pTkpXtp1IKdtPkyeDd9oXfkl",
+    retryQueue: true,
+    addBundledMetadata: true,
+
+  }
+};
+
+
+// Initialize the library
+analytics.initialize(integrationSettings);
 
 const RestaurantsPage = () => {
+  analytics.track('🚀');
   return (
     <section className='restaurants-page-container'>
       <Form />
